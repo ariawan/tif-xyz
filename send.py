1acfb88d7370ec28ce3fc4fa4af8bfb3e75f7b40
jkwbhk <jkwbhk@gmail.com>
2020-03-23 12:03:05 +0700

    added kurs

--- send.py
+++ send.py
@@ -1,25 +1,47 @@
 import requests
+import datetime
 
 x_api_key = '<API-KEY>'          # from damcorp
 group_id = '<GROUP-ID>'   # from damcorp
-
-
-message = 'ini test saja'       # from you
-
-postHeaders = {
-    'Content-type': 'application/json', 
-    'x-api-key': x_api_key
-}
-
 url = 'https://g7omggvlc7.execute-api.us-east-1.amazonaws.com/dev/send_group' # url
 
-json_data = {
-    'group_id': group_id,
-    'message': message
-}
-response = requests.post(url, json=json_data, headers = postHeaders)
+def send_to_group(message):
 
-print("Status code: ", response.status_code)
-print("Printing Entire Post Request")
-print(response.json())
+    postHeaders = {
+        'Content-type': 'application/json', 
+        'x-api-key': x_api_key
+    }
 
+    json_data = {
+        'group_id': group_id,
+        'message': message
+    }
+    response = requests.post(url, json=json_data, headers = postHeaders)
+
+    return response
+
+# print("Status code: ", response.status_code)
+# print("Printing Entire Post Request")
+# print(response.json())
+
+def get_rate_usd(bank):
+
+    url = 'https://kurs.web.id/api/v1/bca'
+
+    return requests.get(url).json()
+
+kurs = get_rate_usd('bca')
+
+message = """
+Kurs USD (BCA)
+
+Jual: Rp. [usd_jual],-
+Beli: Rp. [usd_beli],-
+
+[date_time]
+"""
+message = message.replace('[usd_jual]', f"{int(kurs['jual']):,}")
+message = message.replace('[usd_beli]', f"{int(kurs['beli']):,}")
+message = message.replace('[date_time]', datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+
+print(message)
